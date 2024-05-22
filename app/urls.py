82881b986eef4d6c1792044f322b495edf05434c
Julien Nyambal <Julien.Nyambal@gmail.com>
2024-05-22 13:10:50 +0200

    updating working code

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from app import views
 
 urlpatterns = [
     path("", views.user_list, name = "home"),
+    # path("hello/", views.hello, name = "hello"),
     path('add/', views.add_user, name = "add"),
     path('edit/<id>', views.edit_user, name = "edit"),
     path('delete/<eid>', views.delete_user, name = "delete"),
--- crudexample/settings.py
+++ crudexample/settings.py
@@ -43,15 +43,6 @@ INSTALLED_APPS = [
     "app",
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-       'rest_framework.authentication.BasicAuthentication',
-       'rest_framework.authentication.SessionAuthentication',
-   ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
 
 
 MIDDLEWARE = [
